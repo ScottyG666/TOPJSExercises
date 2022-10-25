6fd73425f651ae8a9fdc1f95ec9e1998ba549f4d
ScottyG666 <Scott_M_G@live.com>
2022-10-24 20:27:46 -0700

    solving the first two exercises.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(word, repetitionCount) {
+    let baseString = '';
+    if (repetitionCount >= 0 ) {
+        for (let i = repetitionCount ; i > 0; i--) {
+            baseString += word;
+        }
+    } else {
+        baseString += 'ERROR'
+    }
 
+    return baseString;
 };
 
 // Do not edit below this line
